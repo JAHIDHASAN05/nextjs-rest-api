26ec57f3bb4eee61a8287a2b0dbdb7771ef91a3b
JAHID HASAN <developerjahid@gmail.com>
2024-06-12 19:28:15 +0600

    Patch method done by params request

---
+++ app/api/hello/[id]/route.js
@@ -0,0 +1,15 @@
+import comment from "../../data/coment";
+
+export async function GET (_request, {params}){
+    const commentId= params.id
+    const coment = comment.find(comment=> comment.id === parseInt(commentId))
+    return Response.json(coment)
+}
+export async function PATCH (request, {params}){
+    const updatedComment= await request.json()
+    const commentId= params.id
+    const commentIndex = comment.findIndex(comment=> comment.id === parseInt(commentId))
+    comment[commentIndex].message=updatedComment.text
+
+    return Response.json(comment[commentIndex])
+}
