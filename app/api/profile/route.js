542181a0d0bef533418ce6ace1ce99c65d407dfc
JAHID HASAN <developerjahid@gmail.com>
2024-06-13 16:40:31 +0600

    set cookie to the browser

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -3,10 +3,14 @@ import { headers } from "next/headers";
 export async function GET (request){
 
     // const requestHeaders= new Headers(request.headers)
-    console.log(requestHeaders);
+    // console.log(requestHeaders);
     const headeList = headers()
     console.log(headeList);
     // console.log(requestHeaders.get('Authorization'))
 
-    return new Response('working profile orute')
+    return new Response('working profile orute', {
+        headers:{
+            'set-cookie' : "theme=dark"
+        }
+    })
 }
